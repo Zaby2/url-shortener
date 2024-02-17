eefaa5c8c38119ffe79c8e280b314ce3483c96c0
Zaby2 <work.acca@yandex.ru>
2024-02-17 21:33:17 +0300

    refactor + RestController added

--- src/main/java/com/microservice/example/conversionservice/urlshortener/controllers/UrlShortenerRestController.java
+++ src/main/java/com/microservice/example/conversionservice/urlshortener/controllers/UrlShortenerRestController.java
@@ -1,7 +1,13 @@
 package com.microservice.example.conversionservice.urlshortener.controllers;
 
+import com.microservice.example.conversionservice.urlshortener.dtoObjects.ShortUrl;
 import com.microservice.example.conversionservice.urlshortener.dtoObjects.UrlDto;
+import com.microservice.example.conversionservice.urlshortener.dtoObjects.UserResponseDto;
+import com.microservice.example.conversionservice.urlshortener.services.UrlServiceImpl;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,10 +18,19 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 public class UrlShortenerRestController {
 
+    @Autowired
+    UrlServiceImpl urlService;
 
+
+    // Error handlers need to be created
+    // Create bean for ResponseEntity for durability
     @PostMapping("/short-url")
-    public String getUrl(@RequestBody UrlDto urlDto) {
-
-        return "";
+    public ResponseEntity<UserResponseDto> getUrl(@RequestBody UrlDto urlDto) {
+        ShortUrl shortUrlToRet = urlService.generateShortUrl(urlDto);
+        UserResponseDto userResponseDto = new UserResponseDto();
+        userResponseDto.setShortUrl(shortUrlToRet.getShortUrl());
+        userResponseDto.setLongUrl(shortUrlToRet.getLongUrl());
+        userResponseDto.setExpirationDate(shortUrlToRet.getExpirationTime());
+        return new ResponseEntity<UserResponseDto>(userResponseDto, HttpStatus.OK);
     }
 }
